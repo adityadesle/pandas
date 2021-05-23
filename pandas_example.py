5fa20f94c28249c8171ef0e43704b5ca5f23ce2a
aditya desle <50102208+adityadesle@users.noreply.github.com>
2021-05-23 20:38:37 +0530

    Add files via upload

---
+++ pandas_example.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 09:38:15 2020
+
+@author: 66IN
+"""
+
+
+import pandas as pd
+
+df = pd.read_csv('Desktop/Python/data/survey_results_public')
+
+print(df)
